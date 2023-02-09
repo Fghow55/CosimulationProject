797366e3dded8df5717941853fa47f6124887ca2
Fghow55 <fghow55@gmail.com>
2023-02-09 14:19:08 +1300

    Opal-RT stuff built

--- DC_Microgrid/models/DC_Microgrid_6/Opcommon/DC_Microgrid_6.sig
+++
@@ -1,8 +0,0 @@
-[AcqGr1]
-1=DC_Microgrid_6/sm_circuit/port1|P_conn|1|1|1||
-2=DC_Microgrid_6/sm_circuit/port2|I_rms|2|1|1||
-nbsignals=2
-[sm_circuit]
-1=DC_Microgrid_6/sc_display/port1|P_load|1|1|1|85|
-2=DC_Microgrid_6/sc_display/port2|V_rms|2|1|1|76|
-nbsignals=2
--- DC_Microgrid/models/ssc_bridge_rectifier/ssc_bridge_rectifier_rt_sm_circuit/OpREDHAWKtarget/ssc_bridge_rectifier_1_sm_circuit_a2b15696_1_ds_external_struct.h
+++
@@ -1,7 +0,0 @@
-/* Simscape target specific file.
- * This file is generated for the Simscape network associated with the solver block 'ssc_bridge_rectifier_1_sm_circuit/sm_circuit/Solver Configuration'.
- */
-
-#ifndef struct__ExternalFunctionStructTag
-#define struct__ExternalFunctionStructTag
-#endif
